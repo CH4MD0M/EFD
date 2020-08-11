0ec88a35ad00a5e976a5a06d2ed89e0e7606d664
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2020-08-11 10:11:45 +0900

    WEBPACK세팅
    sass-loader, css-loader 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,26 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/js/index.js"],
+  // entry files
+  entry: ["@babel/polyfill", "./src/js/index.js", "./src/sass/main.scss"],
 
+  // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
-    path: path.resolve(__dirname, "dist/js"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/index.html",
+    }),
+
+    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
     }),
   ],
 
@@ -28,6 +36,16 @@ module.exports = {
             presets: ["@babel/preset-env"],
           },
         },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader", // compiles Sass to CSS, using Node Sass by default
+        ],
+        exclude: /node_modules/,
       },
     ],
   },
