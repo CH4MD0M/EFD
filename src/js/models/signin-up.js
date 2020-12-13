91b3e4518bcaf076c1d0c32f428434082a6001a7
CH4MD0M <started677@gmail.com>
2020-12-13 22:16:30 +0900

    Update signin-up.js

--- src/js/models/signin-up.js
+++ src/js/models/signin-up.js
@@ -1,12 +1,10 @@
 import { elements } from '../views/base';
+const btnElement = [elements.sign_in_btn, elements.sign_up_btn];
 
-function signUpMode(el) {
+btnElement.map(function (el) {
     if (el) {
         el.addEventListener('click', () => {
             elements.container.classList.toggle('sign-up-mode');
         });
     }
-}
-
-signUpMode(elements.sign_in_btn);
-signUpMode(elements.sign_up_btn);
+});
