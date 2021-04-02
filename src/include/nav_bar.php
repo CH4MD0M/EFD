ab29f9d0b8bcbf2eeaadf243c3161b612494d80f
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2021-04-02 14:47:54 +0900

    네비게이션바 반응형 구현

--- src/include/nav_bar.php
+++ src/include/nav_bar.php
@@ -1,136 +1,110 @@
 <?php require_once 'controllers/authController.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="css/style.css"/>
-    </head>
-    <body>
-        <div class="navigation" id="navBar">
-            <div class="navigation__logo"><span>EFD</span>Korea</div>
-            <nav class="navigation__nav">
-                <ul class="nav">
-                    <!-- Home -->
-                    <li class="nav__item home_active">
-                        <a href="index.php" class="nav__link">
-                            <svg class="home__icon">
-                                <use xlink:href="img/sprite.svg#icon-home"></use>
-                            </svg>
-                            <span>Home</span>
-                        </a>
-                    </li>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <header class="navigation__header">
+      <div class="naivgation__container">
+        <input type="checkbox" name="" id="check" />
 
-                    <!-- 교육안내 -->
-                    <li class="nav__item">
-                        <a href="info.php" class="nav__link">
-                            <svg class="nav__icon">
-                                <use xlink:href="img/sprite.svg#icon-keyboard_arrow_right"></use>
-                            </svg>
-                            <span>교육안내</span>
-                        </a>
-
-                        <ul class="dropdown">
-                            <li class="dropdown__item">
-                                <a href="info.php" class="dropdown__link">
-                                    <span>초등부 과정</span>
-                                </a>
-                            </li>
-
-                            <li class="dropdown__item">
-                                <a href="info2.php" class="dropdown__link">
-                                    <span>중등부 과정</span>
-                                </a>
-                            </li>
-
-                            <li class="dropdown__item">
-                                <a href="info3.php" class="dropdown__link">
-                                    <span>고등부 과정</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <!-- 교육자료소개 -->
-                    <li class="nav__item">
-                        <a href="intro.php" class="nav__link">
-                            <svg class="nav__icon">
-                                <use xlink:href="img/sprite.svg#icon-keyboard_arrow_right"></use>
-                            </svg>
-                            <span>교육자료 소개</span>
-                        </a>
-
-                        <ul class="dropdown">
-                            <li class="dropdown__item">
-                                <a href="intro.php" class="dropdown__link">
-                                    <span>Elementary</span>
-                                </a>
-                            </li>
-
-                            <li class="dropdown__item">
-                                <a href="intro2.php" class="dropdown__link">
-                                    <span>Middle</span>
-                                </a>
-                            </li>
-
-                            <li class="dropdown__item">
-                                <a href="intro3.php" class="dropdown__link"> <span>High</span></a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <!-- 고객지원 -->
-                    <li class="nav__item">
-                        <a href="customer.php" class="nav__link">
-                            <svg class="nav__icon">
-                                <use xlink:href="img/sprite.svg#icon-keyboard_arrow_right"></use>
-                            </svg>
-                            <span>고객 지원</span>
-                        </a>
-
-                        <ul class="dropdown">
-                            <li class="dropdown__item">
-                                <a href="customer.php" class="dropdown__link"> <span>자주묻는 질문</span></a>
-                            </li>
-
-                            <li class="dropdown__item">
-                                <a href="board.php" class="dropdown__link">
-                                    <span>공지사항</span></a
-                                >
-                            </li>
-                        </ul>
-                    </li>
-
-                    <!-- 나의 강의실 -->
-                    <li class="nav__item">
-                        <a href="#" class="nav__link">
-                            <svg class="nav__icon">
-                                <use xlink:href="img/sprite.svg#icon-keyboard_arrow_right"></use>
-                            </svg>
-                            <span>My class</span>
-                        </a>
-                    </li>
-
-                    <!-- 로그인 -->
-                    <li class="nav__item">
-                        <?php 
-                            if(isset($_SESSION['id']) && $_SESSION['verified']){ ?>
-                                <a href="#" class="nav__link username">
-                                    <span> <?php echo $_SESSION['username']; ?> 님  </span>
-                                </a>
-                        <?php
-                            } else { ?>
-                                <a href="signin.php" class="nav__link">
-                                    <svg class="nav__icon">
-                                        <use xlink:href="img/sprite.svg#icon-keyboard_arrow_right"></use>
-                                    </svg>
-                                    <span> 로그인 </span>
-                                </a>
-                        <?php     
-                            }?>
-                    </li>
-                </ul>
-            </nav>
+        <!-- 로고 -->
+        <div class="logo-container">
+          <a href="index.php" class="logo">EFD<span>Korea</span></a>
         </div>
-    </body>
+
+        <!-- 메뉴 -->
+        <nav class="nav-container">
+          <div class="nav-links">
+            <ul class="nav-ul">
+              <!-- 교육안내 -->
+              <li class="nav-link" style="--i: 0.85s">
+                <a href="#">교육안내</a>
+                <!-- dropdown -->
+                <div class="dropdown">
+                  <ul class="dropdown-ul">
+                    <li class="dropdown-link">
+                      <a href="info.php">초등부 과정</a>
+                    </li>
+                    <li class="dropdown-link">
+                      <a href="info2.php">중등부 과정</a>
+                    </li>
+                    <li class="dropdown-link">
+                      <a href="info3.php">고등부 과정</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              <!-- 교육자료 소개 -->
+              <li class="nav-link" style="--i: 1.1s">
+                <a href="#">교육자료 소개</a>
+                <!-- dropdown -->
+                <div class="dropdown">
+                  <ul class="dropdown-ul">
+                    <li class="dropdown-link">
+                      <a href="intro.php">Elementary</a>
+                    </li>
+                    <li class="dropdown-link">
+                      <a href="intro2.php">Middle</a>
+                    </li>
+                    <li class="dropdown-link">
+                      <a href="intro3.php">High</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              <!-- 고객 지원 -->
+              <li class="nav-link" style="--i: 1.35s">
+                <a href="#">고객 지원</a>
+                <!-- dropdown -->
+                <div class="dropdown">
+                  <ul class="dropdown-ul">
+                    <li class="dropdown-link">
+                      <a href="customer.php">자주묻는 질문</a>
+                    </li>
+                    <li class="dropdown-link">
+                      <a href="board.php">공지사항</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              <!-- My class -->
+              <li class="nav-link" style="--i: 1.6s">
+                <a href="#">My class</a>
+              </li>
+
+              <!-- 로그인 -->
+              <li class="nav-link">
+                <?php 
+                    if(isset($_SESSION['id']) && $_SESSION['verified']){ ?>
+                <a href="#" class="username">
+                  <?php echo $_SESSION['username']; ?>
+                  님
+                </a>
+                <?php
+                    } else { ?>
+                <a href="signin.php">
+                  <span> 로그인 </span>
+                </a>
+                <?php     
+                    }?>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div class="hamburger-menu-container">
+          <div class="hamburger-menu">
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </body>
 </html>
